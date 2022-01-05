7be40781836045c70b41ce21280400aeebb5dfed
endsrynt <84342810+endsrynt@users.noreply.github.com>
2022-01-05 16:03:25 +0700

    Add files via upload

---
+++ nama.php
@@ -0,0 +1,18 @@
+<?php
+function nama()
+	{
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "http://ninjaname.horseridersupply.com/indonesian_name.php");
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	$ex = curl_exec($ch);
+	// $rand = json_decode($rnd_get, true);
+	preg_match_all('~(&bull; (.*?)<br/>&bull; )~', $ex, $name);
+	return $name[2][mt_rand(0, 14) ];
+	}
+
+$nama = explode(" ", nama());
+$nama1 = $nama[0];
+$nama2 = $nama[1];
